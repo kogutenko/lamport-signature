5f10cc518b021a04db4d0a29f4d0d0eec7856157
Кто-то <29797602+kogutenko@users.noreply.github.com>
2021-01-06 04:15:33 +0300

    Sign generator added

--- generate.py
+++ generate.py
@@ -4,8 +4,8 @@ from hashlib import sha256
 
 if len(sys.argv) != 2:
     print("Lamport public and private key generator\n")
-    print("Usage: generate.py [key_title]\n")
-    print("Produces two files with names [key_title].public and [key_title].private. Files will be overwritten!\n")
+    print("Usage: generate.py [key_name]\n")
+    print("Produces two files with names [key_name].public and [key_name].private. Files will be overwritten!\n")
     sys.exit()
 
 
@@ -22,18 +22,18 @@ file_name = sys.argv[1]
 
 # Private key
 
-private_file = open(file_name + ".private", "wb")
+private_file = open(file_name + ".private", "w")
 
 for (x, y) in private_list:
-    private_file.write(x.hex().encode() + b" " + y.hex().encode() + b"\n")
+    private_file.write(x.hex() + " " + y.hex() + "\n")
 
 private_file.close()
 
 # Public key
 
-private_file = open(file_name + ".public", "wb")
+private_file = open(file_name + ".public", "w")
 
 for (x, y) in public_list:
-    private_file.write(x.encode() + b" " + y.encode() + b"\n")
+    private_file.write(x + " " + y + "\n")
 
 private_file.close()
---
+++ sign.py
@@ -0,0 +1,71 @@
+import sys
+from hashlib import sha256
+from binascii import unhexlify
+
+if len(sys.argv) != 3:
+    print("Lamport sign generator\n")
+    print("Usage: [document_name] [key_name]\n")
+    print("Notice: private key file should be provided without '.private' extension\n")
+    print("Produces one file with name [document_name].sign")
+    sys.exit()
+
+
+# Reading input files
+
+# Document
+
+document_name = sys.argv[1]
+
+with open(document_name, "rb") as content_file:
+    document_file = content_file.read()
+
+# Private key
+
+key_name = sys.argv[2]
+
+with open(key_name + ".private", "r") as content_file:
+    private_file = content_file.read()
+
+
+# Converting private keys file to binary form
+
+private_file = private_file.split("\n")
+
+private_list = [private_file[i].split(" ") for i in range(256)]
+
+
+# Calculating document hash
+
+document_hash = sha256(document_file).digest()
+
+
+# Exporting sign
+
+sign_list = []
+
+index = 0
+
+# Extracting only needed private keys
+
+for byte in document_hash:
+    
+    bit = 0x80
+    
+    while bit:
+        
+        if bit & byte:
+            sign_list.append(private_list[index][1])
+        else:
+            sign_list.append(private_list[index][0])
+        
+        bit >>= 1
+        index += 1
+
+# Writing to file
+
+sign_file = open(document_name + ".sign", "w")
+
+for sign in sign_list:
+    sign_file.write(sign + "\n")
+
+sign_file.close()
